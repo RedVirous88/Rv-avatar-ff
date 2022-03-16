c46e2d9e7b532f79fcd021496525ffae6ef0213c
RedVirous88 <88195156+RedVirous88@users.noreply.github.com>
2022-03-16 16:05:21 +0200

    Create Rv-ff-script.sh

---
+++ Rv-ff-script.sh
@@ -0,0 +1,14 @@
+#RedVirous
+
+cd
+
+apt update && apt upgrade
+
+termux-setup-storage
+
+cd
+
+cd Rv-avatar-ff
+
+mv assetindexer.PSrlap~2BbZcfMCxyw~2Bt7dClukzYs~3D /sdcard/Android/data/com.dts.freefireth/files/contentcache/Compulsory/android/gameassetbundles/avatar
+
